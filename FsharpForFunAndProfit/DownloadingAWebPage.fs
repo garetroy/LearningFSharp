8381d322f885637d607ddc85d21f1cc18d396b29
Garett Roberts <mygarett@gmail.com>
2020-05-02 00:08:52 -0700

    Added DownloadingAWebPage

---
+++ FsharpForFunAndProfit/DownloadingAWebPage.fs
@@ -0,0 +1,33 @@
+﻿module DownloadingAWebPage
+
+open System.Net
+open System
+open System.IO
+
+let fetchUrl callback url =
+    let req = WebRequest.Create(Uri(url))
+    use resp = req.GetResponse()
+    use stream = resp.GetResponseStream()
+    use reader = new IO.StreamReader(stream)
+    callback reader url
+
+let myCallback (reader:IO.StreamReader) url =
+    let html = reader.ReadToEnd()
+    let html1000 = html.Substring(0, 1000)
+    printfn "Downloaded %s. First 1000 is %s" url html1000
+    html
+
+let google = fetchUrl myCallback "http://google.com"
+
+let bakedIn = fetchUrl myCallback
+
+let sites = ["http://google.com"; 
+                "http://bing.com"; 
+                "http://myspace.com"]
+
+[<EntryPoint>]
+let main argv =
+    sites |> List.map bakedIn 
+    0 // return an integer exit code
+
+
--- FsharpForFunAndProfit/Sorting.fs
+++ FsharpForFunAndProfit/Sorting.fs
@@ -1,4 +1,6 @@
-﻿open System
+﻿module Sorting
+
+open System
 
 let rec quicksort list =
     match list with
@@ -21,10 +23,10 @@ let rec quicksort2 = function
         let smaller,larger = List.partition ((>=) first) rest
         List.concat [quicksort2 smaller; [first]; quicksort2 larger]
         
-[<EntryPoint>]
-let main argv =
-    printfn  "HI"
-    quicksort [1;53;123;4;231;4;2;3] |> printf "%A"
-    quicksort [423;1;422;3;15;3;4] |> printf "%A"
-    0 // return an integer exit code
+//[<EntryPoint>]
+//let main argv =
+//    printfn  "HI"
+//    quicksort [1;53;123;4;231;4;2;3] |> printf "%A"
+//    quicksort [423;1;422;3;15;3;4] |> printf "%A"
+//    0 // return an integer exit code
 

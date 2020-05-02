1e20ba5dbdd83d2b717153743d3e1d4aa08fe580
Garett Roberts <mygarett@gmail.com>
2020-05-01 22:32:17 -0700

    Added problems. Fixed .gitignore.

---
+++ LearningFSharp/Sorting.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+let rec quicksort list =
+    match list with
+    | [] ->
+        []
+    | firstElem::otherElements ->
+        let smallerElements =
+            otherElements
+            |> List.filter (fun e -> e < firstElem)
+            |> quicksort
+        let largerElements = 
+            otherElements
+            |> List.filter (fun e -> e >= firstElem)
+            |> quicksort
+        List.concat [smallerElements; [firstElem]; largerElements]
+
+let rec quicksort2 = function
+    | [] -> []
+    | first::rest ->
+        let smaller,larger = List.partition ((>=) first) rest
+        List.concat [quicksort2 smaller; [first]; quicksort2 larger]
+        
+[<EntryPoint>]
+let main argv =
+    printfn  "HI"
+    quicksort [1;53;123;4;231;4;2;3] |> printf "%A"
+    quicksort [423;1;422;3;15;3;4] |> printf "%A"
+    0 // return an integer exit code
+
---
+++ LearningFSharp/SumOfSquares.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+//https://fsharpforfunandprofit.com/posts/fvsc-quicksort/
+
+module SumOfSquares 
+
+open System
+
+let square x = x * x;
+
+let sumOfSquares n = 
+    [1..n] 
+    |> List.map square 
+    |> List.sum;
+
+let squareF x = x * x;
+
+let sumOfSquareF n =
+    [1.0..n]
+    |> List.map squareF
+    |> List.sum
+
+////[<EntryPoint>]
+//let main argv =
+//    printfn  "HI"
+//    sumOfSquares 4 
+//        |> (printfn "%i")
+//    sumOfSquareF 2.1 
+//        |> (printfn "%f")
+//    0 // return an integer exit code
+
